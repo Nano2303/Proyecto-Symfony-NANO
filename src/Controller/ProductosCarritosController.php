1a5305e7b1b55f03779372de8caf7c79633933c3
Juan <juanignaciobdm@gmail.com>
2024-06-02 18:38:58 +0200

    Ahora se eliminan las existencias que se añaden en el carrito, pero faltaria mejorarlo si el usuario no compra dentro de x dias se les quita la reserva de esa cantidad y tambien separamos la logica del controlador al service

--- src/Controller/ProductosCarritosController.php
+++ src/Controller/ProductosCarritosController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ProductosCarritosService;
 
 class ProductosCarritosController extends AbstractController
 {
@@ -25,19 +26,22 @@ class ProductosCarritosController extends AbstractController
     private $carritoComprasRepository;
     private $entityManager;
     private $productosCarritoRepository;
+    private $productosCarritosService;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         UsuariosRepository $usuariosRepository,
         ProductosRepository $productosRepository,
         CarritoComprasRepository $carritoComprasRepository,
-        ProductosCarritoRepository $productosCarritoRepository
+        ProductosCarritoRepository $productosCarritoRepository,
+        ProductosCarritosService $productosCarritosService
     ) {
         $this->entityManager = $entityManager;
         $this->usuariosRepository = $usuariosRepository;
         $this->productosRepository = $productosRepository;
         $this->carritoComprasRepository = $carritoComprasRepository;
         $this->productosCarritoRepository = $productosCarritoRepository;
+        $this->productosCarritosService = $productosCarritosService;
     }
 
     #[Route('/productos/carritos', name: 'app_productos_carritos')]
@@ -50,7 +54,7 @@ class ProductosCarritosController extends AbstractController
 
 
     #[Route('/carrito/agregar-producto', name: 'carrito_agregar_producto', methods: ['POST'])]
-    public function agregarProducto(Request $request, Session $session): Response
+    public function agregarProductoCarrito(Request $request, Session $session): Response
     //Request tiene que tener el id carrito de compras y el id del producto y cantidad
     {
         if (!$session->has('user_email')) {
@@ -63,63 +67,9 @@ class ProductosCarritosController extends AbstractController
             return new JsonResponse(['error' => 'Usuario no encontrado'], Response::HTTP_FOUND);
         }
 
-        $data = json_decode($request->getContent(), true);
-
-        $carrito_por_user_id = $this->carritoComprasRepository->findByUserId($usuario->getId());
-
-
-        $productos_id = $data['productos_id'] ?? null;
-        $cantidad = $data['cantidad'] ?? null; //Entero todos
-
-        if (!$productos_id || !$cantidad) {
-            return new JsonResponse(['error' => 'Tienes que proporcionar todos los datos '], Response::HTTP_NOT_IMPLEMENTED);
-        }
-        if (!$carrito_por_user_id) {
-            $carrito = new CarritoCompras();
-            $carrito->setUsuarios($usuario);
-        }else {
-            $carrito = $this->carritoComprasRepository->find($carrito_por_user_id);
-        }
-
-        $producto = $this->productosRepository->find($productos_id);
-
-        if (!$producto) {
-            return new JsonResponse(['error' => 'No se ha encontrado este producto'], Response::HTTP_NOT_FOUND);
-        }
-
+        return $this->productosCarritosService->agregarProductoCarrito($request,$usuario);
         
-
-        $productosCarritoExistente = $this->productosCarritoRepository->findOneBy([
-            'carritoCompras' => $carrito,
-            'productos' => $producto
-        ]);
-
-        if ($productosCarritoExistente) {
-            // Si el producto ya existe en el carrito, actualiza la cantidad
-            $productosCarritoExistente->setCantidad($productosCarritoExistente->getCantidad() + $cantidad);
-
-            $total = $producto->getPrecio() * $cantidad;
-
-            $carrito->setTotal($carrito->getTotal() + $total);
-        } else {
-            // Si el producto no existe en el carrito, crea uno nuevo
-            $carritoCompras  = new ProductosCarrito();
-            $carritoCompras->setCarritoCompras($carrito);
-            $carritoCompras->setProductos($producto);
-            $carritoCompras->setCantidad($cantidad);
-            $this->entityManager->persist($carritoCompras);
-
-            $total = $producto->getPrecio() * $cantidad;
-
-            $carrito->setTotal($carrito->getTotal() + $total);
-
-            $this->entityManager->persist($carritoCompras);
-        }
-
-
-        $this->entityManager->persist($carrito);
-        $this->entityManager->flush();
-
-        return new JsonResponse(['Mensaje' => 'Pedido agregado al carrito con exito'], Response::HTTP_OK);
     }
 }
+/**
+ */
