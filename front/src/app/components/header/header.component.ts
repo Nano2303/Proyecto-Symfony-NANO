d3c79619740d6e9dea4a0212372081249f10d507
Angel <angelzmc50@gmail.com>
2024-06-05 23:16:29 +0200

    Boton cerrar sesion

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private _cart: Cart = { items: [] };
   itemsQuantity = 0;
+  isLoggedIn = false;
 
   @Input()
   get cart(): Cart {
@@ -24,7 +28,17 @@ export class HeaderComponent {
       .reduce((prev, curent) => prev + curent, 0);
   }
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private loginService: LoginService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.checkSession();
+  }
 
   getTotal(items: CartItem[]): number {
     return this.cartService.getTotal(items);
@@ -32,5 +46,35 @@ export class HeaderComponent {
 
   onClearCart(): void {
     this.cartService.clearCart();
+  }
+
+  onLogout(): void {
+    this.loginService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+        this.isLoggedIn = false;
+      },
+      error: (error) => {
+        console.error('Error al cerrar sesión:', error);
+      }
+    });
+  }
+
+  checkSession(): void {
+    console.log('checkSession called');
+    this.http.get<{ rol: string }>('http://localhost:8000/comprobar-session').subscribe({
+      next: (response) => {
+        this.isLoggedIn = true;
+        console.log('Hay una sesion iniciada: ' + this.isLoggedIn);
+      },
+      error: (error) => {
+        if (error.status === 404) {
+          this.isLoggedIn = false;
+          console.log('No hay ninguna sesion iniciada: ' + this.isLoggedIn);
+        } else {
+          console.error('Error comprobando la sesión:', error);
+        }
+      }
+    });
   }
 }
