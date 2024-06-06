19ef7073171d9b3069b74b86b77e5d1a9acd5b79
Juan <juanignaciobdm@gmail.com>
2024-06-06 21:00:47 +0200

    sip

--- front/src/app/services/recoverpass/recover-pass.service.ts
+++ front/src/app/services/recoverpass/recover-pass.service.ts
@@ -12,7 +12,7 @@ export class RecoverPassService {
   constructor(private http: HttpClient) { }
 
   recoverPassword(data: { email: string }): Observable<any> {
-    return this.http.post(this.apiUrl, data);
+    return this.http.post(this.apiUrl, data,{ withCredentials: true });
   }
 
   setNewPassword(data: { codigo: string, nueva_contrasena: string }): Observable<any> {
