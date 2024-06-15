6d95525c97cf090474ddc74cf1cc06d197c0fb21
Juan <juanignaciobdm@gmail.com>
2024-06-16 01:52:06 +0200

    cors

--- back/public/index.php
+++ back/public/index.php
@@ -17,12 +17,13 @@ $allowedOrigins = [
 // Configuración CORS dinámica
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 } else {
     header("Access-Control-Allow-Origin: null");
 }
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // Manejar solicitudes OPTIONS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
@@ -30,26 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
 
-// Configuración de cookies de sesión
-if (isset($_SERVER['HTTP_HOST'])) {
-    $domain = $_SERVER['HTTP_HOST'];
-
-    // Ajustar el dominio de la cookie para permitir múltiples TLDs y IP pública
-    if (filter_var($domain, FILTER_VALIDATE_IP)) {
-        $cookieDomain = ''; // No se especifica dominio para la IP pública
-    } else {
-        // Ajustar el dominio para permitir cookies en todos los subdominios del TLD
-        $cookieDomain = '.' . implode('.', array_slice(explode('.', $domain), -2));
-    }
-
-    ini_set('session.cookie_domain', $cookieDomain);
-}
-
-// Asegurarse de que las cookies se envíen solo a través de HTTPS si está habilitado
-ini_set('session.cookie_secure', '0'); // '1' para HTTPS, '0' para HTTP
-ini_set('session.cookie_samesite', 'None'); // Permitir envío entre dominios
-
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
