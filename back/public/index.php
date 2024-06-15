7d4c1ef3423ab425fc3d249731bf1eac7f01ce88
Juan <juanignaciobdm@gmail.com>
2024-06-16 01:28:25 +0200

    cors

--- back/public/index.php
+++ back/public/index.php
@@ -3,24 +3,34 @@
 use App\Kernel;
 
 // Definir orígenes permitidos
-$allowedOrigins = ['http://localhost:4200', 'http://194.164.170.132', 'http://www.synonym-shop.es','http://synonym-shop.es','http://www.synonym-shop.com','http://synonym-shop.com','http://www.synonym-shop.eu','http://synonym-shop.eu',];
+$allowedOrigins = [
+    'http://localhost:4200', 
+    'http://194.164.170.132', 
+    'http://www.synonym-shop.es',
+    'http://synonym-shop.es',
+    'http://www.synonym-shop.com',
+    'http://synonym-shop.com',
+    'http://www.synonym-shop.eu',
+    'http://synonym-shop.eu',
+];
 
 // Configuración CORS dinámica
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-} else {
-    header("Access-Control-Allow-Origin: null");
+    header("Access-Control-Allow-Credentials: true"); // Permitir credenciales (cookies)
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 }
-
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // Manejar solicitudes OPTIONS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(204);
     exit;
 }
+
+// Configuración de cookies de sesión
+ini_set('session.cookie_secure', '1');  // Si estás usando HTTPS
+ini_set('session.cookie_samesite', 'None');  // Permitir envío entre dominios
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
