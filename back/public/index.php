35efff214979992727823abf2970dedd77c700fc
Juan <juanignaciobdm@gmail.com>
2024-06-16 01:39:00 +0200

    cors

--- back/public/index.php
+++ back/public/index.php
@@ -3,7 +3,16 @@
 use App\Kernel;
 
 // Definir orígenes permitidos
-$allowedOrigins = ['http://localhost:4200', 'http://194.164.170.132', 'http://www.synonym-shop.es','http://synonym-shop.es','http://www.synonym-shop.com','http://synonym-shop.com','http://www.synonym-shop.eu','http://synonym-shop.eu',];
+$allowedOrigins = [
+    'http://localhost:4200',
+    'http://194.164.170.132',
+    'http://www.synonym-shop.es',
+    'http://synonym-shop.es',
+    'http://www.synonym-shop.com',
+    'http://synonym-shop.com',
+    'http://www.synonym-shop.eu',
+    'http://synonym-shop.eu',
+];
 
 // Configuración CORS dinámica
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
@@ -22,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
 
-require_once dirname(_DIR_).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
