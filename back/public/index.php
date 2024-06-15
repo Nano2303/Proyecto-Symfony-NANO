2383b2638f4be90e4a5b1a1b121cdaf91840aaeb
Juan <juanignaciobdm@gmail.com>
2024-06-16 01:30:40 +0200

    cors

--- back/public/index.php
+++ back/public/index.php
@@ -3,24 +3,18 @@
 use App\Kernel;
 
 // Definir orígenes permitidos
-$allowedOrigins = [
-    'http://localhost:4200', 
-    'http://194.164.170.132', 
-    'http://www.synonym-shop.es',
-    'http://synonym-shop.es',
-    'http://www.synonym-shop.com',
-    'http://synonym-shop.com',
-    'http://www.synonym-shop.eu',
-    'http://synonym-shop.eu',
-];
+$allowedOrigins = ['http://localhost:4200', 'http://194.164.170.132', 'http://www.synonym-shop.es','http://synonym-shop.es','http://www.synonym-shop.com','http://synonym-shop.com','http://www.synonym-shop.eu','http://synonym-shop.eu',];
 
 // Configuración CORS dinámica
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header("Access-Control-Allow-Credentials: true"); // Permitir credenciales (cookies)
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+} else {
+    header("Access-Control-Allow-Origin: null");
 }
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // Manejar solicitudes OPTIONS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
@@ -28,11 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
 
-// Configuración de cookies de sesión
-ini_set('session.cookie_secure', '1');  // Si estás usando HTTPS
-ini_set('session.cookie_samesite', 'None');  // Permitir envío entre dominios
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(_DIR_).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
